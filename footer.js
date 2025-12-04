978e1de2999f1cdb35c859b670dd25c759c56ef8
Your Name <subhankarrout251@gmail.com>
2025-12-04 21:57:36 +0530

    Refactor code structure for improved readability and maintainability

---
+++ footer.js
@@ -0,0 +1,41 @@
+document.write(
+  `  <footer class="text-light section-dark">
+            <div class="container">
+                <div class="row g-4 align-items-center">
+                    <div class="col-md-12">
+                        <div class="d-lg-flex align-items-center justify-content-between text-center">
+                            <div>
+                                <h3 class="fs-20">Address</h3>
+                                NH 16 NEAR CHIARA DELIGHT BERHAMPUR<br>
+                                Pin 760003
+                            </div>
+                            <div>
+                                <img src="images/logo_white.png" class="w-200px" alt=""><br>
+                                <div class="social-icons mb-sm-30 mt-4">
+                                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                                </div>
+
+                            </div>
+                            <div>
+                                <h3 class="fs-20">Contact Us</h3>
+                                T.- +91 9124555095<br>
+                                M.- chiaraenclave@gmail.com
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="subfooter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            Copyright 2025 - CHIARA ENCLAVE by AKSHYA KUMAR DASH
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>`
+);
---
+++ header.js
@@ -0,0 +1,130 @@
+document.write(
+  `  <header class="transparent has-topbar">
+    <div id="topbar">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="d-flex justify-content-between xs-hide">
+                                <div class="header-widget d-flex">                                    
+                                    <div class="topbar-widget"><a href="#"><i class="icofont-location-pin"></i>NH 16 NEAR CHIARA DELIGHT BERHAMPUR, Pin 760003</a></div>
+                                    <div class="topbar-widget"><a href="#"><i class="icofont-phone"></i>+91 9124555095</a></div>
+                                    <div class="topbar-widget"><a href="#"><i class="icofont-envelope"></i>chiaraenclave@gmail.com</a></div>
+                                </div>
+
+                                <div class="social-icons">
+                                    <a href="#"><i class="fa-brands fa-facebook fa-lg"></i></a>
+                                    <a href="#"><i class="fa-brands fa-x-twitter fa-lg"></i></a>
+                                    <a href="#"><i class="fa-brands fa-youtube fa-lg"></i></a>
+                                    <a href="#"><i class="fa-brands fa-pinterest fa-lg"></i></a>
+                                    <a href="#"><i class="fa-brands fa-instagram fa-lg"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="de-flex sm-pt10">
+                            <div class="de-flex-col">
+                                <!-- logo begin -->
+                                <div id="logo">
+                                    <a href="index.html">
+                                        <img class="logo-main" src="images/logo_white.png" alt="" >
+                                        <img class="logo-scroll" src="images/logo_white.png" alt="" >
+                                        <img class="logo-mobile" src="images/logo_white.png" alt="" >
+                                    </a>
+                                </div>
+                                <!-- logo close -->
+                            </div>
+                            <div class="de-flex-col header-col-mid">
+                                <ul id="mainmenu">
+                                    <li><a class="menu-item" href="index.html">Home</a></li>
+
+
+
+
+                                      <li><a class="menu-item" href="rooms.html">Rooms</a>
+                                        <ul class="mega">
+                                            <li>
+                                                <div class="container">
+                                                    <div class="sb-menu p-4">
+                                                        <div class="row g-3">
+                                                            <div class="col-lg-3">
+                                                                <h4>Rooms</h4>
+                                                                <ul class="no-bg">
+                                                                    <li><a href="rooms.html">All Rooms</a></li>
+                                                                    <li><a href="Dulux.html">Duluxe Rooms Style
+                                                                            2</a></li>
+                                                                    <li><a href="Suite.html">Suite Rooms </a></li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-lg-3 text-center">
+                                                                <div
+                                                                    class="relative hover text-center overflow-hidden soft-shadow rounded-5px">
+                                                                    <a href="Dulux.html" class="d-block">
+                                                                        <div class="d-label">
+                                                                            Best Seller
+                                                                        </div>
+                                                                        <img src="images/form/2.jpeg"
+                                                                            class="w-100 relative hover-scale-1-1"
+                                                                            alt="">
+                                                                    </a>
+
+                                                                    <div
+                                                                        class="abs w-100 h-100 start-0 top-0 hover-bg-color">
+                                                                    </div>
+                                                                </div>
+                                                                <h5 class="mt-2 mb-0">Deluxe Room</h5>
+                                                            </div>
+                                                            <div class="col-lg-3 text-center">
+                                                                <div
+                                                                    class="relative hover text-center overflow-hidden soft-shadow rounded-5px">
+                                                                    <a href="Suite.html">
+                                                                        <div class="d-label">
+                                                                            Featured
+                                                                        </div>
+                                                                        <img src="images/form/6.jpg"
+                                                                            class="w-100 relative hover-scale-1-1"
+                                                                            alt="">
+                                                                    </a>
+
+                                                                    <div
+                                                                        class="abs w-100 h-100 start-0 top-0 hover-bg-color">
+                                                                    </div>
+                                                                </div>
+                                                                <h5 class="mt-2 mb-0">Suite</h5>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </li>
+
+
+
+
+
+
+
+                                    <li><a class="menu-item" href="gallery.html">Gallery</a></li>
+                                    <li><a class="menu-item" href="about.html">About Us</a></li>
+                                    <li><a class="menu-item" href="facilities.html">Facilities</a></li>
+                                    <li><a class="menu-item" href="contact.html">Contact</a></li>
+                                </ul>
+                            </div>
+                            <div class="de-flex-col">
+                                <div class="menu_side_area">          
+                                    <a href="booking-form.html" class="btn-main btn-line">Reservation</a>
+                                    <span id="menu-btn"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>`
+);
